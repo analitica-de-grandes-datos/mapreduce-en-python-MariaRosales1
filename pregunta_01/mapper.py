28f0f89c5126b0b6db8a590d494378847edf0353
Maria Isabel Rosales <mirosaless@unal.edu.co>
2022-06-10 21:42:42 -0500

    Ejercicio 1 resuelto

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+
+def purpose_amount(x):
+    return x[2].strip().replace('\n','')
+
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
